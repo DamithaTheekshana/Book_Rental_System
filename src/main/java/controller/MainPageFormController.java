f1907be1487efd9e4b4bf20a23e0c08b1d3f226f
Damitha Theekshana <theekshanadamitha@gmail.com>
2025-10-28 00:32:29 +0530

    initial commit

---
+++ src/main/java/controller/MainPageFormController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class MainPageFormController {
+
+    @FXML
+    private Button btnAdmin;
+
+    @FXML
+    private Button btnStaff;
+
+    @FXML
+    private ImageView imgAdmin;
+
+    @FXML
+    private ImageView imgLogo;
+
+    @FXML
+    private ImageView imgStaff;
+
+    @FXML
+    void btnAdminOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStaffOnAction(ActionEvent event) {
+
+    }
+
+}
